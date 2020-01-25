0c5d2e9d0493ef7a080f5d714f16cacc88251d2e
AristotleH <53080395+AristotleH@users.noreply.github.com>
2020-01-25 15:00:41 -0800

    Added comments and fixed errors

--- gps_log.py
+++ gps_log.py
@@ -3,16 +3,20 @@ import serial
 import time
 from datetime import datetime
 
+#establish connection with serial input
 uartSerial = serial.Serial("/dev/ttyS0", 9600, timeout=1)
 
+#get current time and generate file name
 current = datetime.now()
 fileName = "/home/pi/GPS/" + current.strftime("%Y-%m-%d_%H-%M-%S_gpsLog.txt")
 
+#create file and write the logging start time to file
 log = open(fileName, "a+")
-log.write("END_LOG: " + current.strftime("%Y-%m-%d_%H-%M-%S")
+log.write("START_LOG: " + current.strftime("%Y-%m-%d_%H-%M-%S"))
 
 while True:
     try:
+        #read a line of data from the serial input
         data = uartSerial.readline()
 
         #if the string of data containing gps data is found
@@ -28,8 +32,9 @@ while True:
             #write the data to the file
             log.write(gpsParsed + "\n")
             time.sleep(1)
+    #when ctrl-c is pressed
     except KeyboardInterrupt:
-        current = datetime.now()
+        #get current time, write logging end time, and stop script
         log.write("END_LOG: " + current.strftime("%Y-%m-%d_%H-%M-%S"))
         print("\nStopping script!")
         exit()
